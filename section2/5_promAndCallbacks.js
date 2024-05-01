132e7365730ad5ad09de05d0c8858e573e575706
Erick Giraldo <erickgiraldo94@gmail.com>
2024-04-30 21:22:13 -0500

    Finish (For real) 5_promAndCallbacks

--- section2/5_promAndCallbacks.js
+++ section2/5_promAndCallbacks.js
@@ -1,19 +1,30 @@
 const manejarAsincronia = (callback, promise) =>{
-    promise.then(() => callback()).catch(() => {
-        alert("Promise not fulfilled and callback not executed")
-    })
+    promise.then(response => callback(response)).catch(response => callback(response))
 }
 
 let myPromise = new Promise((resolve, reject) =>{
     let ans = confirm("Do you want to cancel or confirm the promise?")
     if(ans){
-        setTimeout(() => {resolve()}, 2000)
+        setTimeout(() => {resolve("resolved")}, 2000)
     }
-    setTimeout(() => {reject()}, 2000)
+    setTimeout(() => {reject("rejected")}, 2000)
 })
 
-let callBack = () =>{
-    alert("Promise fulfilled and callback executed")
+let callBack = (response) =>{
+    let msg = "Promise "
+    if(response === "resolved"){
+        msg += response + " and callback executed"
+    }else{
+        msg += response + " and callback NOT executed"
+    }
+    alert(msg)
 }
 
 manejarAsincronia(callBack, myPromise)
+
+// Answering questions
+// 1. What happens if I change the time of promise resolution from 5 seconds to 1 second? R/ The callback will take less to be called, since this time has always to be finished first than the callback
+
+// 2. How does the function behave if the promise is rejected instead of resolved? R/ The callback is not called and a print message with "promised not resolved" is propmted to the user.
+
+//3. Can I modify the function so the callback handles different type of information depending on the result of the promise R/ Yes, I can do that by returning a response from the promise in reject() or resolve(), then pass it as a parameter for the callback in the "then" or "except"
