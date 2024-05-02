b469b23864335fbbf94121905961d25b8ac24c82
Erick Giraldo <erickgiraldo94@gmail.com>
2024-05-02 17:40:19 -0500

    Finish 10_eventLoopVisualization

---
+++ section4/10_eventLoopVisualization.js
@@ -0,0 +1,11 @@
+// Answering the questions
+
+//1. Which tasks are considered macrotasks and which microtasks?
+
+//2. How are related the macrotasks and the microtasks in the event loop?
+
+//3. What happens when a microtask generates a new macrotask inside it?
+
+//4. How are the promises and the setTimeout handled in relation to the event loop?
+
+// (Answered in the word attached)
